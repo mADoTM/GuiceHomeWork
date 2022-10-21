5d400342c925499c91dd4dffd6083856ee803a60
Максим Долженко <maxim_2003@mail.ru>
2022-10-21 23:10:01 +0300

    File is cleaning

--- app/src/main/java/ru/vk/utils/FileUtils.java
+++ app/src/main/java/ru/vk/utils/FileUtils.java
@@ -1,22 +1,31 @@
 package ru.vk.utils;
 
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashSet;
 
 public class FileUtils {
+    private final static HashSet<String> filesUsingStatus = new HashSet<>();
+
     public static void createFileWithText(String text) throws IOException {
         String fileName = new SimpleDateFormat("yyyyMMdd").format(Calendar.getInstance().getTime()) + ".txt";
 
-        File file = new File(fileName);
 
-        if(!file.exists()) {
-            file.createNewFile();
+        File file = new File(fileName);
+        var path = file.toPath();
+
+        var operation = StandardOpenOption.TRUNCATE_EXISTING;
+        if (file.exists() && filesUsingStatus.contains(fileName)) {
+            operation = StandardOpenOption.APPEND;
+        } else {
+            filesUsingStatus.add(fileName);
         }
-        Files.write(Paths.get(fileName), (text + "\n").getBytes(), StandardOpenOption.WRITE);
+        Files.write(path, (text + "\n").getBytes(), operation);
+        System.out.println("end file log");
     }
 }
